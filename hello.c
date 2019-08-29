717d3dd078910b8601e58b36308ea435b56a5d64
liuai2637 <54672932+liuai2637@users.noreply.github.com>
2019-08-29 08:44:49 -0500

    Create hello.c
    Add a line to print my major

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Sunny Liu
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sunny Liu\n");
+
+printf("Mathematics")
+
+  return 0;
+}
